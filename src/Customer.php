7ad1e236ffe367119cbcb1f5534df0b55c6a83cc
arief2020 <msyaifullahalarief@gmail.com>
2022-09-19 00:34:52 +0700

    contoh library composer hello

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace musarif\Belajar;
+
+class Customer{
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name): string{
+        return "Hello $name, My Name is $this->name";
+    }
+}
